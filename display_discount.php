b95b5d1daf681a378ea3e8e9bb0ad17e1ab863f9
balakumarhs <bs443@njit.edu>
2017-02-10 23:33:05 -0500

    added b and i tag

--- display_discount.php
+++ display_discount.php
@@ -18,13 +18,13 @@ $final_price = $list_price - $discount_amount;
         <h1>This page is under construction</h1>
 
         <label>Product Description:</label>
-        <span><?php echo $productdis; ?></span><br>
+        <span><b> <?php echo $productdis; ?></b></span><br>
 
         <label>List Price:</label>
         <span><?php echo "$".$list_price ; ?></span><br>
 
         <label>Standard Discount:</label>
-        <span><?php echo $discount_percent."%"; ?></span><br>
+        <span><i><?php echo $discount_percent."%"; ?></i></span><br>
 
         <label>Discount Amount:</label>
         <span><?php echo "$".$discount_amount; ?></span><br>
